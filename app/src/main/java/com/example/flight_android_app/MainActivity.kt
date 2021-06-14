260eeff58c19f049a08fa5f56e1ce6e1904b9ccd
AMIT ILOVITCH <amitilovitch@gmail.com>
2021-06-14 18:41:21 +0300

    update text inputs

--- app/src/main/java/com/example/flight_android_app/MainActivity.kt
+++ app/src/main/java/com/example/flight_android_app/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.flight_android_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    var ip: EditText? = null;
+    var port :EditText? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ip = findViewById(R.id.textField);
+        port = findViewById(R.id.IP);
     }
+
 }
